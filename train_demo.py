c0441593e41293e462673b8e10c40e6487643963
AniketGurav <aniket.diat@gmail.com>
2018-12-06 20:54:14 +0530

    Update train_demo.py
    Few questions about variables and functions

--- train_demo.py
+++ train_demo.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 15 14:06:27 2017
+@author: AAg
 
-@author: ray
+in which format data is required???
 """
 
 from src.gen_data import generator_data
@@ -21,7 +22,10 @@ import os
 def cross_entropy( y_true, y_pred ) :
     return -K.sum(y_true * K.log(y_pred), axis=1 )
 
-
+'''
+    why 0.5 is subtracted???
+    
+'''
 def preprocess_img(img):
     img = im.imresize(img,(480,576,),interp='nearest')
     #print target.shape
@@ -30,35 +34,62 @@ def preprocess_img(img):
         img = -img
     return img
 
-
+'''
+    below function only calls train model and does not define any network
+'''
 def train(model,data_list,batch_size=2,border_perc=0.16,backend='tf'):
+    
+    '''
+        data_list: ???
+    '''
     with open(data_list,'r') as f:
         info = f.readlines()
     epoch = 1000
+    
     steps = len(info) // batch_size
+    
     for i in (xrange(1,epoch+1)):
+        
         print '-'*15 , 'Epoch %d' % i , '-'*15
+
         for j in tqdm(xrange(steps)):
             if backend == 'tf':
                 batch_img = np.zeros(shape=(batch_size,480,576,3))
                 batch_label = np.zeros(shape=(batch_size,480,576,3))
             else:
+                '''
+                    will it work tensorflow is backend 
+                '''
                 batch_img = np.zeros(shape=(batch_size,3,480,576))
                 batch_label = np.zeros(shape=(batch_size,3,480,576))
             count = 0
             size = batch_size
+            '''
+                what is exactly size below???
+            '''
             if j == steps - 1:
                 size = batch_size + len(info) % batch_size
             pwd = os.getcwd()
+            
             for k in range(size):
                 img_path,label_path = info[j*batch_size+k].split()
                 try:
                     img = im.imread(os.path.join(pwd,img_path),mode='RGB')
+                    
+                    '''
+                        does data support pascal voc format??
+                    '''
+                    
                     if label_path.endswith('.xml'):
                         target = read_xml_target(label_path,border_perc)
                     elif label_path.endswith('.json'):
                         target= read_json_target(label_path,border_perc)
+                    
+                    '''
+                        what is target is it segmented image???
+                    '''
                     target = im.imresize(target,(480,576),interp='nearest')
+                    
                     if backend == 'tf':
                         batch_img[count] = preprocess_img(img)
                         batch_label[count] = target / 255.
@@ -70,6 +101,10 @@ def train(model,data_list,batch_size=2,border_perc=0.16,backend='tf'):
                 except:
                     continue
             print('batch size is %d' % count)
+            
+            '''
+                why below is commented I think neeed to uncomment for training??
+            '''
             #model.train_on_batch(batch_img[:count],batch_label[:count].astype('uint8'))
             if j % 10 == 0:
                 print('the test loss is %f' % model.evaluate(batch_img[0:8],batch_label[0:8].astype('uint8')))
@@ -82,10 +117,16 @@ def train(model,data_list,batch_size=2,border_perc=0.16,backend='tf'):
 
 
 if __name__ == '__main__':
-    model = create_multi_res_model()
-    model.compile('adam',cross_entropy)
+    
+    
+    model = create_multi_res_model() # load architecture 
+    model.compile('adam',cross_entropy) 
 
     train_mode = 'sequence'
+    '''
+        what is difference between sequence and non sequence mode???
+    '''
+    
     if train_mode == 'sequence':
         model.load_weights('model_weight/multi_res.h5')
         train(model,'src/idcard_list.list',12,backend='th')
